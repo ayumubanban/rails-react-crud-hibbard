d03e81287ec90c09ff7b1bcdea0af32a3f7054dd
ayumu <ayumuban@outlook.com>
2019-09-16 21:16:00 +0900

    first ver

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -3,14 +3,39 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 class EventList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            searchTerm: ''
+        };
+
+        this.searchInput = React.createRef();
+        this.updateSearchTerm = this.updateSearchTerm.bind(this);
+    }
+
+    updateSearchTerm(){
+        this.setState({ searchTerm: this.searchInput.current.value });
+    }
+
+    matchSearchTerm(obj){
+        const {
+            id, published, created_at, updated_at, ...rest
+        } = obj;
+        const { searchTerm } = this.state;
+
+        return Object.values(rest).some(
+            value => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1,
+        );
+    }
+
     renderEvents(){
         const { activeId, events } = this.props;
         // * 大きい順に並べ替えてくれるっぽい
-        events.sort(
-            (a, b) => new Date(b.event_date) - new Date(a.event_date)
-        );
+        const filteredEvents = events
+            .filter(el => this.matchSearchTerm(el))
+            .sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
-        return events.map(event => (
+        return filteredEvents.map(event => (
             <li key={event.id}>
                 <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
                     {event.event_date}
@@ -24,7 +49,19 @@ class EventList extends React.Component{
     render(){
         return (
             <section className="eventList">
-                <h2>Events</h2>
+                <h2>
+                    Events
+                    <Link to="/events/new">New Event</Link>
+                </h2>
+
+                <input
+                    className="search"
+                    placeholder="Search"
+                    type="text"
+                    ref={this.searchInput}
+                    onKeyUp={this.updateSearchTerm}
+                />
+
                 <ul>{this.renderEvents()}</ul>
             </section>
         );
